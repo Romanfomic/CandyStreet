e494ca6515b388c9fe8094fd664797ec5527fb88
Roman Fomichev <romanfomic@gmail.com>
2023-12-15 16:46:27 +0300

    Add sort function

--- test_case_1/Main.java
+++ test_case_1/Main.java
@@ -1,6 +1,10 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 class Main {
     public static void main(String[] args) {
@@ -8,9 +12,10 @@ class Main {
         Map<String, String> fileList = new HashMap<>();
 
         listFiles(rootDirectory, fileList);
+        fileList = sortFiles(fileList);
 
         for (Map.Entry<String, String> entry : fileList.entrySet()) {
-            System.out.println("File: " + entry.getKey() + ", Path: " + entry.getValue());
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 
@@ -29,5 +34,48 @@ class Main {
                 fileList.put(file.getName(), file.getAbsolutePath());
             }
         }
+    }
+    private static Map<String, String> sortFiles(Map<String, String> fileList) {
+        Map<String, String> sortedFileMap = new TreeMap<>(fileList);
+        Map<String, String> newSortedMap = sortedFileMap;
+
+        for (Map.Entry<String, String> entry : sortedFileMap.entrySet()) {
+            try{
+                File file = new File(entry.getValue());
+
+                Scanner scanner = new Scanner(file);
+
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if (line.contains("require")) {
+                        String message = line.replaceAll(".*'(.*)'.*", "$1");
+                        int lastIndex = message.lastIndexOf('/');
+                        String fileKey = message.substring(lastIndex + 1);
+
+                        Map<String, String> newFileMap = new LinkedHashMap<>();
+
+                        // Переменная, которая указывает, был ли уже добавлен элемент hieghestFile
+                        boolean hieghestFile = false;
+
+                        for (Map.Entry<String, String> entry_2 : newSortedMap.entrySet()) {
+                            // Добавляем hieghestFile перед файлом, который ниже в списке
+                            if (!hieghestFile && entry_2.getKey().equals(entry.getKey())) {
+                                newFileMap.put(fileKey, sortedFileMap.get(fileKey));
+                                hieghestFile = true;
+                            }
+
+                            // Добавляем текущий элемент в новую Map
+                            newFileMap.put(entry_2.getKey(), entry_2.getValue());
+                            newSortedMap = newFileMap;
+                        }
+                    }
+                }
+
+                scanner.close();
+            } catch(FileNotFoundException e) {
+                System.out.println("File is not found exeption: " + e.getMessage());
+            }
+        }
+        return newSortedMap;
     }
 }
